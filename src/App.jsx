c61ff3126405d36d65b7d065d23915f6cf580a7c
Athira Santhosh <tech.athiras@gmail.com>
2023-12-30 06:24:19 -0800

    landing component added and completed with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Landing from "./components/Landing/Landing";
 
 function App() {
   return (
     <div>
+      {/* <BrowserRouter> */}
       <Header />
+      <Landing />
+      {/* <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes> */}
+      {/* </BrowserRouter> */}
     </div>
   );
 }
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,19 +1,22 @@
-import "./HomePage.scss";
-import landingImg from "../../assets/images/landing-img";
+import "./Landing.scss";
+import landingImg from "../../assets/images/landing-img.svg";
 
-function HomePage() {
+function Landing() {
   return (
-    <>
+    <div className="landing">
       <div className="landing__top">
         <h1 className="landing__title">.ambience</h1>
         <p className="landing__description">
           Discover uniquely crafted building materials from artisans around the
-          world. Spice up your space with Handcrafted Materials Delivery Service
+          world. Spice up your space with{" "}
+          <strong>Handcrafted Materials Delivery Service</strong>
         </p>
       </div>
       <div className="landing__bottom">
         <section className="landing__more-details">
-          <img src={landingImg} alt="" className="landing__img" />
+          <section className="landing__img-container">
+            <img src={landingImg} alt="" className="landing__img" />
+          </section>
           <p className="landing__detailed-decription">
             Experience the joy of beautiful living spaces with our artistic
             material finishes. Order online and let us deliver, authentic
@@ -21,8 +24,8 @@ function HomePage() {
           </p>
         </section>
       </div>
-    </>
+    </div>
   );
 }
 
-export default HomePage;
+export default Landing;
