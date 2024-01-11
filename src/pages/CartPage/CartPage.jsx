351ae5a4a62dd3d361ec422725d3c3de8db1aa42
Athira Santhosh <tech.athiras@gmail.com>
2024-01-11 03:05:14 -0800

    styling completed for cartpage

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom"; // Don't forget to import Link
+import { Link } from "react-router-dom";
 import "./CartPage.scss";
 
 const SERVER_URL = "http://localhost:8080";
@@ -19,17 +19,17 @@ function CartPage() {
       {cart.map((item) => (
         <div className="cart" key={item.material.id}>
           <h3 className="cart__title">Order Summary</h3>
-          <p>
-            <img
-              src={item.material.image}
-              alt={item.material.name}
-              className="cart__image"
-            />
-          </p>
+
+          <img
+            src={item.material.image}
+            alt={item.material.name}
+            className="cart__image"
+          />
+
           <h3 className="cart__material-name">{item.material.name}</h3>
           <section className="cart__quantity-price-container">
-            <p className="cart__selected-quantity">{`Quantity (${item.material.quantity})`}</p>
-            <h3 className="cart__selected-price">{`$${item.material.price}`}</h3>
+            <p className="cart__selected-quantity">{`Quantity (${item.quantity})`}</p>
+            <h3 className="cart__selected-price">{`${item.material.price}`}</h3>
           </section>
           <section className="cart__button-container">
             <Link
@@ -42,25 +42,33 @@ function CartPage() {
             >
               Shop More
             </Link>
-            <Link
-              to="/checkout"
-              className="cart__button"
-              onClick={() => {
-                // Handle Proceed to Pay button click
-                console.log("Proceed to Pay clicked");
-              }}
-            >
-              Proceed to Pay
-            </Link>
           </section>
           <section className="cart__subtotal-container">
             <p className="cart__subtotal-title">Subtotal</p>
             <p className="cart__subtotal-amount">{item.material.price}</p>
-            <p className="cart__subtotal-title">Shipping</p>
+          </section>
+          <section className="cart__shipping-container">
+            <p className="cart__shipping-title">Shipping</p>
             <p className="cart__shipping-amount">$20.00</p>
-            <p className="cart__subtotal-title">Total</p>
+          </section>
+          <section className="cart__tax-container">
+            <p className="cart__tax-title">Tax</p>
             <p className="cart__tax-amount">$7.00</p>
           </section>
+          <section className="cart__total-container">
+            <p className="cart__total-title">Total</p>
+            <p className="cart__total-amount">$30.99</p>
+          </section>
+          <Link
+            to="/checkout"
+            className="cart__button"
+            onClick={() => {
+              // Handle Proceed to Pay button click
+              console.log("Proceed to Pay clicked");
+            }}
+          >
+            Proceed to Pay
+          </Link>
         </div>
       ))}
     </>
